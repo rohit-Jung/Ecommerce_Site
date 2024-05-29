49b83482b232e69b9c2e43f95dda14667f0ab485
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2024-05-29 16:09:14 +0545

    export components

---
+++ src/app/Components/index.ts
@@ -0,0 +1,7 @@
+import Footer from "./Footer/Footer";
+import NavBar from "./Header/NavBar";
+import HomeBanner from "./HomeBanner";
+import Container from "./Container";
+import ProductCard from "./Products/ProductCard";
+
+export { NavBar, Footer, HomeBanner, Container, ProductCard };
