acd72bb9b711a2b99eb83ef4b273fb9c6105e223
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2024-06-18 20:57:40 +0545

    layout for admin Nav

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+import React from "react";
+import { AdminNav } from "../Components";
+
+export const metadata: Metadata = {
+  title: "E-commerce App Admin",
+  description: "E-commerce App Admin Dashboard on Next",
+};
+
+interface AdminLayoutProps {
+  children: React.ReactNode;
+}
+const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <AdminNav />
+      {children}
+    </div>
+  );
+};
+
+export default AdminLayout;
